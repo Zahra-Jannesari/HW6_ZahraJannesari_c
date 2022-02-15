054cb92f5b2a397b8e90c37d1c08f6c3c7acb097
Zarisa <jannesari.zarisa@gmail.com>
2022-02-15 23:32:55 +0330

    Initial commit:main repository for hw6

---
+++ app/src/main/java/com/zarisa/hw6_zahrajannesari/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.zarisa.hw6_zahrajannesari
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioGroup
+import android.widget.Toast
+import com.zarisa.hw6_zahrajannesari.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() ,RadioGroup.OnCheckedChangeListener {
+    lateinit var binding: ActivityMainBinding
+    var sharedpreferences: SharedPreferences? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+        sharedpreferences = getSharedPreferences("Information", Context.MODE_PRIVATE)
+        binding.RadioGroupGender.setOnCheckedChangeListener(this)
+        binding.buttonRegister.setOnClickListener { registerButtonClick() }
+        binding.buttonShowInfo.setOnClickListener{showInfoButtonClick()}
+        binding.buttonHideInfo.setOnClickListener{hideInfoButtonClick()}
+    }
+    override fun onCheckedChanged(p0: RadioGroup?, p1: Int) {
+        val editor = sharedpreferences!!.edit()
+        when(p1){
+            R.id.radioButton_Female -> {
+                editor.putString("Gender", "Female")
+                editor.apply()
+                return
+            }
+            R.id.radioButton_Male ->{
+                editor.putString("Gender","Male")
+                editor.apply()
+                return
+            }
+        }
+    }
+    private fun registerButtonClick(){
+        Toast.makeText(this,"ok", Toast.LENGTH_LONG).show()
+        val editor = sharedpreferences!!.edit()
+            editor.putString("fullName", binding.textFieldFullName.text.toString())
+            editor.apply()
+            editor.putString("Username", binding.textFieldUsername.text.toString())
+            editor.apply()
+            editor.putString("Email", binding.textFieldEmail.text.toString())
+            editor.apply()
+            editor.putString("Password", binding.textFieldPassword.text.toString())
+            editor.apply()
+    }
+    private fun showInfoButtonClick() {
+        binding.textViewFullName.text= sharedpreferences?.getString("fullName", "")
+        binding.textViewUserName.text=sharedpreferences?.getString("Username", "")
+        binding.textViewEmail.text=sharedpreferences?.getString("Email", "")
+        binding.textViewPassword.text=sharedpreferences?.getString("Password", "")
+        binding.textViewGenderInfo.text=sharedpreferences?.getString("Gender", "")
+        binding.LinearLayoutInfo.visibility= View.VISIBLE
+    }
+    private fun hideInfoButtonClick(){
+        binding.LinearLayoutInfo.visibility= View.INVISIBLE
+    }
+}
